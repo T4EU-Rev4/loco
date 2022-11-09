2099febe14ee29595bd8c9bc0720f1db5bc64535
bbs-tghs <m.busser@bbstghs.de>
2022-11-09 17:51:13 +0100

    Test_MQTT_2.ino

--- README.md
+++ README.md
@@ -9,3 +9,14 @@ https://www.emqx.com/en/blog/esp32-connects-to-the-free-public-mqtt-broker
 Use the tool MQTT client tool MQTT X mentioned in the section "Required IoT components"
 
 
+## Test_MQTT_2
+We register some more topics and show, how they can be seperated in the callback routine callback
+The topics are organized in a struct, where a bus adress is assigned to every topic.
+These structs are organized in an array of struct, so in case of receiving a topic is is enough
+to compare the topics and read the assigned adress of the device (wagon).
+getIntFromString() read the first, second, third,... in value from the received payload.
+c=3;v=7 is fine  but not necessary.  3;7 will also work.
+see: https://forum.arduino.cc/t/int-aus-string-extrahieren/143685   thanks to jurs and serenifly
+
+Next step: Clean up code, move to VSCode, implement I2C for wagon control
+
---
+++ Test_MQTT_2/Test_MQTT_2.ino
@@ -0,0 +1,120 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+#define NROFTOPICS 5
+
+typedef struct {
+  char    *topic;    //the topic, we want to register
+  uint8_t adr;       //the assigned adress of the bus device
+} TTopic;
+
+TTopic allTopics[NROFTOPICS];
+
+ 
+void initTopics() {
+  allTopics[0].topic = "T4EU/DE";      allTopics[0].adr = 0;
+  allTopics[1].topic = "T4EU/DE/DE";   allTopics[1].adr = 49;
+  allTopics[2].topic = "T4EU/DE/DK";   allTopics[2].adr = 94;
+  allTopics[3].topic = "T4EU/DE/HR";   allTopics[3].adr = 17;
+  allTopics[4].topic = "T4EU/DE/UA";   allTopics[4].adr = 21;
+}
+
+// WiFi
+const char *ssid = "IBB"; // Enter your WiFi name
+const char *password = "pc6sWPuV2YyFO5kSJKpW";  // Enter WiFi password
+
+// MQTT Broker
+const char *mqtt_broker = "broker.emqx.io";
+
+const char *mqtt_username = "emqx";
+const char *mqtt_password = "public";
+const int mqtt_port = 1883;
+
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup() {
+  Serial.begin(115200);         // Set software serial baud to 115200;
+  initTopics();
+  WiFi.begin(ssid, password);   // connecting to a WiFi network
+  while (WiFi.status() != WL_CONNECTED) {
+     delay(500);
+     Serial.println("Connecting to WiFi..");
+  }
+  Serial.println("Connected to the WiFi network");
+ 
+  client.setServer(mqtt_broker, mqtt_port);    //connecting to a mqtt broker
+  client.setCallback(callback);
+  while (!client.connected()) {
+     String client_id = "esp32-client-";
+     client_id += String(WiFi.macAddress());
+     Serial.printf("The client %s connects to the public mqtt broker\n", client_id.c_str());
+     if (client.connect(client_id.c_str(), mqtt_username, mqtt_password)) {
+         Serial.println("Public emqx mqtt broker connected");
+     } else {
+         Serial.print("failed with state ");
+         Serial.print(client.state());
+         delay(2000);
+     }
+ }
+ // publish and subscribe
+ client.publish( allTopics[0].topic, "Hi EMQX I'm ESP32 ^^");
+ for( int i=0; i<NROFTOPICS; i++ ) {
+  client.subscribe( allTopics[i].topic ); 
+ }
+
+ }
+
+void myMQTTReceived( uint8_t adr, int cmd, int val ) {
+  Serial.print( "Adr=" );   Serial.print( adr );
+  Serial.print( "  cmd=" );   Serial.print( cmd );
+  Serial.print( "  val=" );   Serial.println( val );
+}
+
+//https://forum.arduino.cc/t/int-aus-string-extrahieren/143685
+//thanks to jurs
+int getIntFromString (char *stringWithInt, int num)
+{
+  char *tail; 
+  while (num>0)
+  {
+    num--;
+    while ((!isdigit (*stringWithInt))&&(*stringWithInt!=0)) stringWithInt++;
+    tail=stringWithInt;
+
+    while ((isdigit(*tail))&&(*tail!=0)) tail++;
+    if (num>0) stringWithInt=tail;
+  }
+
+  return atoi(stringWithInt);
+}
+
+
+void callback(char *topic, byte *payload, unsigned int length) {
+// Serial.print("Message arrived in topic: ");
+// Serial.println(topic);
+// Serial.print("Message:");
+// for (int i = 0; i < length; i++) {
+//     Serial.print((char) payload[i]);
+// }
+// Serial.println(length);
+
+  for (int i=0; i<NROFTOPICS; i++ ) {
+    int cmd, val;
+    if ( strcmp( topic, allTopics[i].topic )==0 ){
+//      Serial.print("Adr=");
+//      Serial.println( allTopics[i].adr );
+      cmd = getIntFromString( (char*)payload, 1 );   
+      val = getIntFromString( (char*)payload, 2 );
+      myMQTTReceived( allTopics[i].adr, cmd, val );
+      break;
+    }
+  }
+ Serial.println("-----------------------");
+}
+
+void loop() {
+ client.loop();
+}
+
